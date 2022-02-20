8ed085aa955a3c30e6821d269364540b728a7c5f
Shivani <shivanikogta8@gmail.com>
2022-02-20 13:09:27 -0500

    Add project files

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Shivani Kogta on 2/18/22.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onSignin(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func onSignup(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+}
+
